233a2e95c293e62d2b5b38d470cb507566ef94f1
Adrian Pardini <github@tangopardo.com.ar>
2011-09-26 13:38:57 -0300

    fixed output format for net

--- traste_renum.py
+++ traste_renum.py
@@ -9,7 +9,7 @@ out = []
 
 for l in sch:
     if 'traste%i' in l:
-        out.append(l.replace('%i', '%02i' % idx))
+        out.append(l.replace('%i', '%02i:1' % idx))
         idx += 1
     else:
         out.append(l)
