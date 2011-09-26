9f8baebf6ea9eb1d2f154b53fa38781117839cbb
Adrian Pardini <github@tangopardo.com.ar>
2011-09-26 12:33:15 -0300

    add helper script to renumber frets

---
+++ traste_renum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+sch = open(sys.argv[1]).readlines()
+
+idx = 0;
+out = []
+
+for l in sch:
+    if 'traste%i' in l:
+        out.append(l.replace('%i', '%02i' % idx))
+        idx += 1
+    else:
+        out.append(l)
+
+open('out'+sys.argv[1], 'wb').write(''.join(out))
